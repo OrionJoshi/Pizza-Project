8e1d5e1dc21c24064a9ba457a56278ea83d3213d
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-21 23:37:36 +0545

    header and footer added

---
+++ add.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <?php include('templates/header.php'); ?>
+
+        <section class="container grey-text">
+            <h4 class="center">Add a Pizza</h4>
+        </section>
+
+    <?php include('templates/footer.php'); ?>
+
+    
+</html>
