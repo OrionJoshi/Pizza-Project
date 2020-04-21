2f8f2bacf40f2adc6d0fbd0054474835745f9d68
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-21 22:56:14 +0545

    navbar title added

--- templates/header.php
+++ templates/header.php
@@ -4,4 +4,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 </head>
 <body class="grey lighten-4">
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="#" class="brand-logo brand-text">Pizza Project</a>
+        </div>
+    </nav>
     
