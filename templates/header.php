a0bcce38d0989c5b83492a731b648793e9ca5b8b
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-21 23:02:32 +0545

    add pizaa link added

--- templates/header.php
+++ templates/header.php
@@ -7,6 +7,9 @@
     <nav class="white z-depth-0">
         <div class="container">
             <a href="#" class="brand-logo brand-text">Pizza Project</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li><a href="#" class="btn brand z-depth-0">Add Pizza</a></li>
+            </ul>
         </div>
     </nav>
     
