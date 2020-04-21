08edca774752ad77235496b39b6f722c11b849b8
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-21 22:42:24 +0545

    body class added

--- templates/header.php
+++ templates/header.php
@@ -3,5 +3,5 @@
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 </head>
-<body>
+<body class="grey lighten-4">
     
