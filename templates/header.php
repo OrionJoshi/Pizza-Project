294fdb4c83cc20a347341129941a35f4e3ded2cb
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-21 23:05:43 +0545

    brand class css styling added

--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,11 @@
     <title>Pizza Project</title>
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style>
+        .brand{
+            background: #cdd09c !important;
+        }
+    </style>
 </head>
 <body class="grey lighten-4">
     <nav class="white z-depth-0">
