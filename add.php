8c1b7cdc74ddc2953752fb352d1af6c4c9118131
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-22 18:59:55 +0545

    form input field added

--- add.php
+++ add.php
@@ -5,6 +5,15 @@
 
         <section class="container grey-text">
             <h4 class="center">Add a Pizza</h4>
+            <form action="" class="white" method="">
+                <label for="">Your Email:</label>
+                <input type="email" name="email">
+                <label for="">Pizza Title:</label>
+                <input type="text" name="title">
+                <label for="">Ingredient(comma separated):</label>
+                <input type="text" name="ingredients">
+           
+            </form>
         </section>
 
     <?php include('templates/footer.php'); ?>
