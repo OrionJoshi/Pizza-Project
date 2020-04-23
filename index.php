d98ff55bf0f7c9303daad4e3552b4bf911a0ee61
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-23 23:17:25 +0545

    displaying pizzas title and ingredients

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     }
 
     //Write query for all prizzas
-    $sql = 'SELECT title, ingredients, id FROM pizzas';
+    $sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
     //make query and get result
     $result = mysqli_query($conn,$sql);
@@ -27,6 +27,23 @@
 <html lang="en">
 
     <?php include('templates/header.php'); ?>
+
+    <h4 class="center grey-text">Pizzas!</h4>
+    <div class="container">
+        <div class="row">
+            <?php foreach($pizzas as $pizza){?>
+                <div class="col s6 md3">
+                    <div class="card z-depth-0">
+                        <div class="card-content center">
+                            <h6><?php echo htmlspecialchars($pizzas['title']);?></h6>
+                            <div><?php echo htmlspecialchars($pizza['ingredients']);?></div>
+                        </div>
+                    </div>
+                </div>
+            <?php }?>
+        </div>
+    </div>
+
     <?php include('templates/footer.php'); ?>
 
     
