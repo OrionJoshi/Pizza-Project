6de170e98db5e4ae86b03e9ce3ceef03b88eb2d0
khageshwor <khageshwarjoshi03@gmail.com>
2020-04-24 13:02:22 +0545

    delete button added

--- details.php
+++ details.php
@@ -33,6 +33,12 @@
             <p><?php echo date($pizza['created_at']); ?></p>
             <h5>Ingredients:</h5>
             <p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+
+            <!-- Delete Form -->
+            <form action="details.php" method ="POST">
+                <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id']; ?>">
+                <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+            </form>
         <?php else: ?>
 
             <h5>No Such Pizza Exist!</h5>
